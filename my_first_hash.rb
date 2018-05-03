96e487dc2d13002695620b9d47e30e06e204fe45
corytveten <corytveten@gmail.com>
2018-05-03 18:33:49 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = {
-    "Stone Age" => "worker placement"
+    "Stone Age" => "worker placement",
     "Lords of Waterdeep" => "worker placement"
   }
 
